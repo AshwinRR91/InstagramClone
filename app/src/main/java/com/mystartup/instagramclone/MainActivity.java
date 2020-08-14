dbc60843cb45b2bcad3f7ef214cd7ca2850af066
Ashwin <achchu.rai@gmail.com>
2020-08-14 13:54:15 +0530

    Updated codes to set and get data to and from the server for Instagram Clone

--- app/src/main/java/com/mystartup/instagramclone/MainActivity.java
+++ app/src/main/java/com/mystartup/instagramclone/MainActivity.java
@@ -4,37 +4,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView helloWorld;
+
+    private EditText namePerson;
+    private EditText nameRocket;
+    private EditText nameSinger;
+    private Button saveButton;
+    private Button fetchButton;
+    private String str;
+    private TextView getText;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        helloWorld = findViewById(R.id.hello_word);
-        helloWorld.setOnClickListener(new View.OnClickListener() {
+        namePerson = findViewById(R.id.editTextTextPersonName);
+        nameRocket = findViewById(R.id.rocket);
+        nameSinger = findViewById(R.id.singer);
+        fetchButton = findViewById(R.id.fetch_details);
+        saveButton = findViewById(R.id.save_details);
+        fetchButton = findViewById(R.id.fetch_details);
+        getText = findViewById(R.id.get_text);
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ParseObject boxer = new ParseObject("Boxer");
-                boxer.put("PunchSpeed", 200);
-                boxer.saveInBackground(new SaveCallback() {
+
+                ParseObject Likes = new ParseObject("Likes");
+                Likes.put("PersonName",namePerson.getText().toString());
+                Likes.put("RocketName",nameRocket.getText().toString());
+
+                Likes.put("SingerName",nameSinger.getText().toString());
+                Likes.saveInBackground(new SaveCallback() {
                     @Override
                     public void done(ParseException e) {
-                        if(e!=null){
-                            Toast.makeText(MainActivity.this,"Saved in Server",Toast.LENGTH_LONG).show();
+                        if(e != null){
+                        Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+                    }}
+                }); }
+        });
+
+        fetchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                str = "";
+                ParseQuery<ParseObject> getDetails = ParseQuery.getQuery("Likes");
+                getDetails.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if (e == null) {
+                            for (ParseObject pq : objects) {
+                                str += pq.get("PersonName") + "" + pq.get("SingerName") + "" + pq.get("RocketName");
+                            }
+                            getText.setText(str);
+                        } else {
+                            Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
                         }
-                    }
-                });
+                    }});
             }
         });
     }
-
-
 }
