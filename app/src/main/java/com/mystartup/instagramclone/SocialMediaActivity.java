f6ec4fc12abb7597d968116a55b5c288bcd7bb66
Ashwin <achchu.rai@gmail.com>
2020-08-15 16:55:11 +0530

    Tab Adapter Added, Page Viewer Added running as desire succesfully.

--- app/src/main/java/com/mystartup/instagramclone/SocialMediaActivity.java
+++ app/src/main/java/com/mystartup/instagramclone/SocialMediaActivity.java
@@ -1,14 +1,33 @@
 package com.mystartup.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class SocialMediaActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+    private ViewPager mViewPager;;
+    private TabLayout mTabLayout;
+    TabAdapter mTabAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+        setTitle("CrAcKeRs");
+        mToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(mToolbar);
+        mViewPager = findViewById(R.id.view_pager);
+        mTabAdapter = new TabAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(mTabAdapter);
+        mTabLayout = findViewById(R.id.tab_layout);
+        mTabLayout.setupWithViewPager(mViewPager);
+
     }
+
 }
