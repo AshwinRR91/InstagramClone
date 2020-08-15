541183aa4111bf46878e4734b0505ef8ab438eae
Ashwin <achchu.rai@gmail.com>
2020-08-15 15:46:49 +0530

    Instagram clone.. Profile and User tab added. Toolbar added

--- app/src/main/java/com/mystartup/instagramclone/LoginActivity.java
+++ app/src/main/java/com/mystartup/instagramclone/LoginActivity.java
@@ -2,13 +2,55 @@ package com.mystartup.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginUserName;
+    private EditText loginPassword;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginUserName = findViewById(R.id.login_username);
+        loginPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logInInBackground(loginUserName.getText().toString(), loginPassword.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if(user!=null){
+                            switchToSocialMediaActivity();
+                        } }}); }});
     }
+
+    private void switchToSocialMediaActivity(){
+        Intent intent = new Intent(LoginActivity.this,SocialMediaActivity.class);
+        startActivity(intent);
+    }
+
+    private void onLoginRootLayoutTapped(View view){
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        try {
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        catch (Exception e){
+            Log.i("Error LoginActivity", e.getMessage());
+        }
+
+        }
 }
--- app/src/main/java/com/mystartup/instagramclone/MainActivity.java
+++ app/src/main/java/com/mystartup/instagramclone/MainActivity.java
@@ -4,21 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Switch;
 import android.widget.Toast;
 
-import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -37,10 +32,14 @@ private Button signUpButton;
         userName = findViewById(R.id.user_name);
         loginEmail = findViewById(R.id.email_id);
         password = findViewById(R.id.password);
-        loginButton = findViewById(R.id.login_button);
+        loginButton = findViewById(R.id.switch_to_login_button);
         signUpButton = findViewById(R.id.signup_buton);
         loginButton.setOnClickListener(this);
         signUpButton.setOnClickListener(this);
+        if(ParseUser.getCurrentUser()!= null){
+            switchToSocialMediaActivity();
+        }
+
 
     }
 
@@ -50,20 +49,53 @@ private Button signUpButton;
         switch (view.getId()){
 
             case R.id.signup_buton:
-                final ParseUser parseUser = new ParseUser();
-                parseUser.setUsername(userName.getText().toString());
-                parseUser.setEmail(loginEmail.getText().toString());
-                parseUser.setPassword(password.getText().toString());
-                parseUser.signUpInBackground(new SignUpCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if(e != null){
-                            Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+
+                if((userName.getText().toString().equals("") || loginEmail.getText().toString().equals("==")) || (password.getText().toString().equals("=="))) {
+                    Toast.makeText(MainActivity.this, "Please enter the correct credentials", Toast.LENGTH_LONG).show();
+
+                }
+                else{
+                    final ParseUser parseUser = new ParseUser();
+                    parseUser.setUsername(userName.getText().toString());
+                    parseUser.setEmail(loginEmail.getText().toString());
+                    parseUser.setPassword(password.getText().toString());
+                    parseUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e != null) {
+                                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                Toast.makeText(MainActivity.this, "You have been signed up", Toast.LENGTH_LONG).show();
+                            }
                         }
-                    }
-                });
+                    });
+                }
                 break;
+
+            case R.id.switch_to_login_button:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+
 
         }
     }
+    public void rootLayoutTapped(View view){
+
+        InputMethodManager inputMethodManager =(InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        try {
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        catch (Exception e ){
+            Log.i("Error",e.getMessage());
+        }
+
+
+    }
+    private void switchToSocialMediaActivity(){
+        Intent intent = new Intent(MainActivity.this,SocialMediaActivity.class);
+        startActivity(intent);
+    }
+
 }
